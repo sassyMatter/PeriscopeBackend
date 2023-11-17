adcec77b241b87536dc488f18de74f3c8a2becb1
Nirbhay <nirbhay11.singh@ril.com>
2023-11-17 10:35:28 +0530

    changes for user space apis:

--- src/main/java/com/app/models/Configurations.java
+++ src/main/java/com/app/models/Configurations.java
@@ -1,11 +1,13 @@
 package com.app.models;
 
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class Configurations {
 
     public String memory;
---
+++ src/main/java/com/app/utils/ProjectMapper.java
@@ -0,0 +1,38 @@
+package com.app.utils;
+
+import com.app.models.Configurations;
+import com.app.models.Project;
+import com.app.models.payload.response.ProjectData;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Component
+public class ProjectMapper {
+
+    public List<ProjectData> mapToProjectDataList(List<Project> projects) {
+        return projects.stream()
+                .map(ProjectMapper::mapToProjectData)
+                .collect(Collectors.toList());
+    }
+
+    public static ProjectData mapToProjectData(Project project) {
+        ProjectData projectData = new ProjectData();
+        projectData.setProjectName(project.getProjectName());
+        projectData.setImageUrl(project.getImageURL());
+        projectData.setConfigurations(project.getConfigurations());
+        projectData.setIsRunning(false);
+        return projectData;
+    }
+
+    public Project mapProjectDataToProject(ProjectData projectData){
+        Project project = new Project();
+        project.setProjectName(projectData.getProjectName());
+        project.setConfigurations(projectData.getConfigurations());
+        return project;
+    }
+
+}
+
