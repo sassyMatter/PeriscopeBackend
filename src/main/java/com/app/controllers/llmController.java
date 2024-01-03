2e913eb2447a6e50d2cb354969a9b79746d976b7
Nirbhay <nirbhay11.singh@ril.com>
2024-01-03 10:53:11 +0530

    changes

---
+++ src/main/java/com/app/controllers/llmController.java
@@ -0,0 +1,34 @@
+package com.app.controllers;
+
+
+import com.app.models.payload.response.MetaDataResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import com.app.services.llmService;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/auth")
+public class llmController {
+
+
+
+    @Autowired
+    llmService llmService;
+
+
+//    sign-up so that apis can hit it without
+    @PostMapping("/get-response")
+    public MetaDataResponse connectWithModel(@RequestBody String prompt){
+
+       return  MetaDataResponse
+               .builder()
+               .data(llmService.getResponseForPrompt(prompt))
+               .messageCode("processed prompt")
+               .httpStatus(HttpStatus.OK)
+               .build();
+
+    }
+}
