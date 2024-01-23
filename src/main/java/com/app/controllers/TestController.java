cc7f1aeaf7b909b0974be75eb4f891dd0b428efe
Harshit <Harshit8.Singh@ril.com>
2024-01-23 12:10:33 +0530

    changes

--- src/main/java/com/app/controllers/TestController.java
+++ src/main/java/com/app/controllers/TestController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins="*")
-@RequestMapping("/api/auth" )
+@RequestMapping("/api/auth/")
 @Slf4j
 public class TestController {
 
