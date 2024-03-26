612fda170facc9e13ecc120833a875dde314fc72
sangam12-08 <Sangam.Barnwal@ril.com>
2024-03-26 21:37:29 +0530

    apis integration for run and build button

--- src/main/java/com/app/controllers/OrchestrationController.java
+++ src/main/java/com/app/controllers/OrchestrationController.java
@@ -66,78 +66,13 @@ public class OrchestrationController {
 ////        log.info("dockerFileContent {}",dockerfileContent);
 ////        writeDockerfile(dockerfileContent, dockerpath);
 //        bringUpContainers(tempDockerComposePath);
-        List<String> command = List.of("cat",dockerComposeFile);
-        ProcessBuilder processBuilder = new ProcessBuilder(command);
-//        processBuilder.directory(new File(dockerComposeFile));
-        // Step 2: Start the process
-        Process process = processBuilder.start();
-
-        // Step 3: Read the output
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-
-        // Wait for the process to complete
-        int exitCode = process.waitFor();
-        log.info("Exit code{}",exitCode);
-        System.out.println("Exit code: " + exitCode);
 
 
-//        List<String> listFilesCommand = List.of("pwd");
-//        ProcessBuilder listFilesProcessBuilder = new ProcessBuilder(listFilesCommand);
-//        listFilesProcessBuilder.directory(new File(dockerComposeFile));
-//        Process listFilesProcess = listFilesProcessBuilder.start();
-//
-//        BufferedReader listFilesReader = new BufferedReader(new InputStreamReader(listFilesProcess.getInputStream()));
-//        while ((line = listFilesReader.readLine()) != null) {
-//            System.out.println(line);
-//        }
-//
-//        int listFilesExitCode = listFilesProcess.waitFor();
-//        System.out.println("List files exit code: " + listFilesExitCode);
+
 
         return ResponseEntity.ok("Project loaded successfully");
     }
-    public String readDockerfile(String sourcePath) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(sourcePath)));
-    }
 
-    public Path writeToTemporaryDockerComposeFile(String content) throws IOException {
-        Path tempFile = Files.createTempFile("docker-compose-", ".yml");
-        Files.write(tempFile, content.getBytes());
-        log.info("tempfile {}",tempFile);
-        return tempFile;
-    }
-    public void bringUpContainers(Path dockerComposePath) throws IOException {
-        System.out.println("Docker Compose file path: " + dockerComposePath.toString());
-        String destPath=dockerComposePath.toString();
-        log.info("destPath{}",destPath);
-        String destContent=readDockerfile(destPath);
-        log.info("content {}",destContent);
-
-
-        if (!Files.exists(dockerComposePath)) {
-            System.out.println("Docker Compose file does not exist at the specified path.");
-        }
-        else{
-            log.info("dockercomposeexecutable {}",dockerComposeExecutable);
-            try {
-                ProcessBuilder processBuilder = new ProcessBuilder(dockerComposeExecutable,"-f", destPath, "up", "-d");
-
-
-                Process process = processBuilder.start();
-
-                process.waitFor();
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Docker Compose file exists at the specified path.");
-        }
-
-
-    }
 
 
 }
